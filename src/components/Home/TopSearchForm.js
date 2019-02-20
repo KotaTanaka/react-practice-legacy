579433f11047a031bc4fa2850917ad2790c044eb
KotaTanaka <te11k.tanaka@icloud.com>
2019-02-21 02:17:52 +0900

    feat: トップ画面デザイン: 斜め背景

--- src/components/Home/TopSearchForm.js
+++ src/components/Home/TopSearchForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// Layout
+// Style
 import Card from '@material-ui/core/Card';
 import Input from '@material-ui/core/Input';
 import PlaceOutlined from '@material-ui/icons/PlaceOutlined';
@@ -15,25 +16,31 @@ class TopSearchForm extends React.Component {
   render() {
     return (
       <div className="Home-top">
-        <h1 className="Home-title">
-          あなたが使える<nobr className="text-bolder">Wi-Fi</nobr>が見つかる
-        </h1>
+        <div className="Home-header">
+          <Link to="/"><h1>(Logo)</h1></Link>
+        </div>
 
-        <Card>
-          <div className="Home-form">
-            <h2 className="text-lighter">
-              近くのWi-Fiを探す
-            </h2>
-            <div className="Home-text-row">
-              <p>エリア</p><p>こだわり条件</p>
+        <div className="Home-top-content">
+          <h1 className="Home-top-title">
+            あなたが使える<nobr className="text-bolder">Wi-Fi</nobr>が見つかる
+          </h1>
+
+          <Card>
+            <div className="Home-top-form">
+              <h2 className="text-lighter">
+                近くのWi-Fiを探す
+              </h2>
+              <div className="Home-text-row">
+                <p>エリア</p><p>こだわり条件</p>
+              </div>
+              <PlaceOutlined />
+              <Input placeholder="渋谷"></Input>
+              <PowerOutlined />
+              <Input placeholder="電源あり"></Input>
+              <SearchIcon />
             </div>
-            <PlaceOutlined />
-            <Input placeholder="渋谷"></Input>
-            <PowerOutlined />
-            <Input placeholder="電源あり"></Input>
-            <SearchIcon />
-          </div>
-        </Card>
+          </Card>
+        </div>
       </div>
     );
   }
